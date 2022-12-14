096aed3b3abf0321d98d78a1389526a4eaebda77
chushi <tingchushi@hotmail.com>
2022-12-15 02:29:44 +0800

    first commit

--- apps/server/server.js
+++ apps/server/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 // process.env.NODE_ENV => production or undefined
 
 // middleware
-app.use(cors());
+// app.use(cors());
 app.use(express.json()); // req.body
 const  {isTokenValid} = require('./controllers/userLogin');
 
